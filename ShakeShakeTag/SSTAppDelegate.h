f08286cc46b33b12604312079f9cccec6feaaece
bryantung <bryantung89@gmail.com>
2012-11-14 00:56:56 +0800

    Initial Commit

---
+++ ShakeShakeTag/SSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSTAppDelegate.h
+//  ShakeShakeTag
+//
+//  Created by Bryan Tung on 11/14/12.
+//  Copyright (c) 2012 BIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShakeShakeTag/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShakeShakeTag
+//
+//  Created by Bryan Tung on 11/14/12.
+//  Copyright (c) 2012 BIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSTAppDelegate class]));
+    }
+}
